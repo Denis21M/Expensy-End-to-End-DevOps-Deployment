2451cfe3b67c435a47d2d03f8f1d85353ecd06b4
Denis21M <baconchinazom@gmail.com>
2025-05-08 14:27:53 +0200

    initial commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   type        = string
   description = "Azure region"
-  default     = "easteurope"
+  default     = "northeurope"
 }
 
 variable "aks_cluster_name" {
